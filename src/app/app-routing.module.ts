bf26d93bbcabcc82de9c76e5f9d593902b83a21d
Max Flores <max@maxflores.dev>
2023-12-25 09:34:38 -0600

    chore: add error fallback page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
+import { ErrorComponent } from './pages/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component:HomeComponent
+  },
+  {
+    path: '**',
+    title: 'Oops!',
+    component:ErrorComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="bg-violet-300 p-2">
-      <h1>Welcome to {{ title }}</h1>
-    </div>
-  `,
   standalone: false,
+  template: `
+    <main class="">
+      <aside class="bg-violet-300 p-2 h-screen">
+        <h1>Welcome to {{ title }}</h1>
+      </aside>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styles: `
+    main {
+      display: flex;
+    }
+  `
 })
-
 export class AppComponent {
   title = 'The Jungle';
 }
