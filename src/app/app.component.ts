c9411a6dcdb8044505d0edda9270172316af0082
Max Flores <max@maxflores.dev>
2023-12-26 03:20:37 -0600

    chore: add self-hosted fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: false,
   template: `
-    <main class="">
-      <aside class="bg-violet-300 p-2 h-screen">
+    <main class="flex">
+
+      <aside class="bg-cvg-400 p-2 h-screen">
         <h1>Welcome to {{ title }}</h1>
       </aside>
-      <router-outlet></router-outlet>
+
+      <router-outlet />
+
     </main>
-  `,
-  styles: `
-    main {
-      display: flex;
-    }
   `
 })
 export class AppComponent {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cvg: {
+          400: '#1E1F21',
+          300: '#545D68',
+          200: '#C5CED6',
+          100: '#E8EBEF',
+        }
+      }
+    },
   },
   plugins: [],
 }
