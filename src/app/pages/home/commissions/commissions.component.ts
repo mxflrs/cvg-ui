502c39bf73ec73f3472e589839ec5fdfb0418b9f
Max Flores <max@maxflores.dev>
2024-09-17 05:17:29 -0600

    chore: finetune merchandasize section

--- src/app/pages/home/commissions/commissions.component.ts
+++ src/app/pages/home/commissions/commissions.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Artworks } from 'src/app/core/models/artworks.interface';
-import { sanityImage } from 'src/app/core/models/sanity-image.interface';
+import { ImageBuilderService } from 'src/app/services/image-builder.service';
 import { ContentWrapperComponent } from 'src/app/shared/components/content-wrapper.component';
 
 @Component({
@@ -13,4 +13,25 @@ import { ContentWrapperComponent } from 'src/app/shared/components/content-wrapp
 })
 export class CommissionsComponent {
   @Input() artworks: Artworks[] = [];
+  public currentIndex = 0;
+
+  constructor(private imageBuilder: ImageBuilderService) {}
+
+  imageUrl(id: string) {
+    return this.imageBuilder.image(id).url();
+  }
+
+  onNextImage() {
+    const next = this.artworks.pop();
+    if (next) {
+      this.artworks.unshift(next);
+    }
+  }
+
+  onPrevImage() {
+    const last = this.artworks.shift();
+    if (last) {
+      this.artworks.push(last);
+    }
+  }
 }
